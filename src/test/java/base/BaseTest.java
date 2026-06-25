a6e1366217267dc30fadbd075939eacb848eb4ee
Mmanare <maruapulam@gmail.com>
2026-06-25 20:06:26 +0200

    Fix: Configure EdgeDriver with headless mode for CI environments

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,22 +2,29 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     public WebDriver driver;
     @BeforeMethod
     public void setUp(){
-        driver = new EdgeDriver();
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        
+        driver = new EdgeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
 
     }
   //  @AfterMethod
-   public void tearDown() throws InterruptedException {
-        Thread.sleep(1000);
-       driver.quit();
-    }
+    public void tearDown() throws InterruptedException {
+         Thread.sleep(1000);
+        driver.quit();
+     }
 
 
 }
